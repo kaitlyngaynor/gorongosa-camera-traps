a30078d2c79128e89ae36016afc186eb92ab7125
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-03-04 11:01:35 -0800

    New script for calculating distances between grid center points and actual camera locations

---
+++ scripts/01-cleaning/09-calculate-distance-camera-gridcenter.R
@@ -0,0 +1,17 @@
+library(here)
+library(geosphere)
+
+points <- read.csv(here::here("gis", "gridcenter_vs_camera_locations.csv"))
+
+points$distance <- NA
+
+for (i in 1:nrow(points)) {
+  points$distance[i] <- distm(c(points[i,4], points[i,3]), c(points[i,6], points[i,5]), fun = distHaversine)
+}
+
+summary(points$distance)
+hist(points$distance)
+sort(points$distance)
+
+# median of 30.3 meters from center point, mean of 50.5
+# max = 275.37
