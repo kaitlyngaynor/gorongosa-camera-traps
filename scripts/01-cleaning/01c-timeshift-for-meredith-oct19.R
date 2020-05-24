254db8d5de0182155c54c5121ffb65f4ddd67521
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-05-24 14:43:24 -0700

    Add directory structure

--- scripts/01-cleaning/01c-timeshift-for-meredith-oct19.R
+++ scripts/01-cleaning/01c-timeshift-for-meredith-oct19.R
@@ -3,17 +3,24 @@ library(camtrapR)
 # I recommend making a copy of the two folders to be shifted somewhere else, just in case anything goes wrong
 
 # set working directory as the directory that contains the folder with the camera folders in it
-# in other words, [working_directory] > "To_fix" > [camera folders named "B07" and "H07"]
-setwd("~/Desktop")
+# here is what the directory structure should look like:
+# — Working directory
+# --- To_fix
+# ----- B07
+# ------- B07_R1
+# ----- H07
+# ------- H07_R1
+
+setwd("")
 
 timeShiftTable <- data.frame(folder = c("B07", "H07"),
                              sign = c("+", "+"),
                              offset = c("0:0:1685 10:8:0", "0:0:1609 11:18:0"))
 
-timeshift_run <- timeShiftImages(inDir                = "To correct",
-                                 timeShiftTable       = timeShiftTable,
-                                 stationCol           = "folder",
-                                 hasCameraFolders     = FALSE,
-                                 timeShiftColumn      = "offset",
-                                 timeShiftSignColumn  = "sign"
+timeShiftImages(inDir                = "To correct",
+                timeShiftTable       = timeShiftTable,
+                stationCol           = "folder",
+                hasCameraFolders     = FALSE,
+                timeShiftColumn      = "offset",
+                timeShiftSignColumn  = "sign"
 )
